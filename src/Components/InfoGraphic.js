ddb69fcff9608107533cbb09f550b152a7b92882
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2021-12-27 18:14:43 +0100

    Fixed sizes a bit for smaller screens than 1200px

--- src/Components/InfoGraphic.js
+++ src/Components/InfoGraphic.js
@@ -87,24 +87,27 @@ const InfoGraphic = ({ name }) => {
 
   return (
     <div className='infoGraphic'>
-      <div className='infoGraphic__title'>
-        <h1>{name}</h1>
-      </div>
-      <div className='infoGraphic__content'>
-        <div className='infoGraphic__content__text'>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            euismod, nisi vel consectetur interdum, nisl nunc aliquet nunc, eget
-            egestas nisl nunc euismod nunc. Pellentesque habitant morbi
-            tristique senectus et netus et malesuada fames ac turpis egestas.
-          </p>
+      <div className='infographic-container'>
+        <div className='infoGraphic__title'>
+          <h1>{name}</h1>
         </div>
-        <img
-          src='https://via.placeholder.com/300'
-          alt='Strawberry'
-          className='infoGraphic__content__image'
-        />
+        <div className='infoGraphic__content'>
+          <div className='infoGraphic__content__text'>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              euismod, nisi vel consectetur interdum, nisl nunc aliquet nunc,
+              eget egestas nisl nunc euismod nunc. Pellentesque habitant morbi
+              tristique senectus et netus et malesuada fames ac turpis egestas.
+            </p>
+          </div>
+          <img
+            src='https://via.placeholder.com/300'
+            alt='Strawberry'
+            className='infoGraphic__content__image'
+          />
+        </div>
       </div>
+
       <div className='carContainer'>
         {
           // 1 mile = 1.60934 km
